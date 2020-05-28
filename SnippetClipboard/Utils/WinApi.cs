8786bda77a436870c030cbcb45fd731f0ce82797
Andreas Furster <andreas.furster@hotmail.com>
2020-05-28 15:32:33 +0200

    Initial c# version

---
+++ SnippetClipboard/Utils/WinApi.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace SnippetClipboard.Utils
+{
+    public class WinApi
+    {
+        [DllImport("user32.dll")]
+        public static extern bool GetCaretPos(out System.Drawing.Point lpPoint);
+    }
+}
